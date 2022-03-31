8427051bb5368cb77b943a36975fe7413e7e6f7c
paulleerentveld <paul@veldnet.com.au>
2022-03-31 16:09:29 +1000

    add class for lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your Circle class here
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    set diameter(value) {
+        this.radius = value / 2;
+    }
+    set circumference(value) {
+       this.radius = (value / Math.PI) / 2 
+    }
+    set area(value) {
+        this.radius = Math.sqrt((value / Math.PI));
+        
+    }
+    get diameter() {
+        return this.radius * 2;
+    }
+    get circumference() {
+        return this.diameter * Math.PI;
+    }
+    get area() {
+        return Math.PI * (this.radius**2);
+    }
+    
+}
+
